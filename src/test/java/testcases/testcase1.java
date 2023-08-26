5c04dbe5399f4ef3f1670291831159aeacc0a69c
mubarak.shaik <mbshaik960@gmail.com>
2023-08-26 18:30:02 +0530

    testcase1 added

---
+++ src/test/java/testcases/testcase1.java
@@ -0,0 +1,18 @@
+package testcases;
+
+public class testcase1 
+{
+ public static void main(String[] args)
+ {
+	int number1=5;
+	int number2=4;
+	int sum = addNum(number1,number2);
+	System.out.println(sum);
+ }
+ 
+ public static int addNum(int num1, int num2)
+ {
+	 int sum1 = num1+num2;
+	 return sum1;
+ }
+}
